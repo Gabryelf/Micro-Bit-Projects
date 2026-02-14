8f642896f1bc9fdb97ef32bd10a02ffbc4965237
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-02-14 11:11:11 +0300

    создано приложение - шар предсказаний

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    count = randint(1, 7)
+})
+let count = 0
+count = 0
 basic.forever(function () {
-	
+    if (count == 1) {
+        basic.showString("Yes")
+    }
+    if (count == 2) {
+        basic.showString("No")
+    }
+    if (count == 3) {
+        basic.showString("Most likely yes")
+    }
+    if (count == 4) {
+        basic.showString("Most likely no")
+    }
+    if (count == 5) {
+        basic.showString("Exactly yes")
+    }
+    if (count == 6) {
+        basic.showString("Exactly no")
+    }
+    if (count == 7) {
+        basic.showString("Not definitely")
+    }
 })
